a80b6d2e131b7ec82b40d825459c4e0e47479825
ClemensDaCoder <madincubus@hotmail.com>
2013-05-24 16:36:39 +0200

    replaced Articletest with BasketRow

--- SmartShopper/src/at/jku/smartshopper/listitems/ArticleListAdapter.java
+++ SmartShopper/src/at/jku/smartshopper/listitems/ArticleListAdapter.java
@@ -3,31 +3,24 @@ package at.jku.smartshopper.listitems;
 import java.util.List;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnFocusChangeListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.content.DialogInterface;
 import at.jku.smartshopper.client.R;
+import at.jku.smartshopper.objects.BasketRow;
 
-public class ArticleListAdapter extends ArrayAdapter<Articletest> {
+public class ArticleListAdapter extends ArrayAdapter<BasketRow> {
 
-	private List<Articletest> items;
+	private List<BasketRow> items;
 	private int layoutResourceId;
 	private Context context;
 	ArticlerHolder holder;
 	
-	public ArticleListAdapter(Context context, int layoutResourceId, List<Articletest> items) {
+	public ArticleListAdapter(Context context, int layoutResourceId, List<BasketRow> items) {
 		super(context, layoutResourceId, items);
 		this.layoutResourceId = layoutResourceId;
 		this.context = context;
@@ -61,13 +54,13 @@ public class ArticleListAdapter extends ArrayAdapter<Articletest> {
 	
 
 	private void setupItem(ArticlerHolder holder) {
-		holder.name.setText(holder.article.getName());
-		holder.value.setText(String.valueOf(holder.article.getValue()));
+		holder.name.setText(holder.article.getArticleName());
+		holder.value.setText(String.valueOf(holder.article.getPrice()));
 		holder.quantity.setText(String.valueOf(holder.article.getQuantity()));
 	}
 
 	public static class ArticlerHolder {
-		Articletest article;
+		BasketRow article;
 		TextView quantity;
 		TextView name;
 		TextView value;
