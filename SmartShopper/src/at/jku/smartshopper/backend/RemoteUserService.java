9441b8bfb8ee0b9fb70e106fa4ee7f81106a29b6
ClemensDaCoder <madincubus@hotmail.com>
2013-05-27 22:45:08 +0200

    added implemenation of RequestProvider and RemoteUserService

--- SmartShopper/src/at/jku/smartshopper/backend/RemoteUserService.java
+++ SmartShopper/src/at/jku/smartshopper/backend/RemoteUserService.java
@@ -1,5 +1,13 @@
 package at.jku.smartshopper.backend;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 import at.jku.smartshopper.objects.User;
 
 public class RemoteUserService implements IUserService {
@@ -7,10 +15,30 @@ public class RemoteUserService implements IUserService {
 	@Override
 	public User getUser(String username, String password) {
 		// TODO Auto-generated method stub
-		User u = new User();
-		u.setUsername(username);
-		u.setPassword(password);
-		return u;
+//		User u = new User();
+//		u.setUsername(username);
+//		u.setPassword(password);
+//		return u;
+		
+		
+		RequestProvider provider = new RequestProvider();
+		HttpHeaders header = provider.getHttpHeader();
+		HttpEntity<String> requestEntity = new HttpEntity<String>(username, header);
+		
+		RestTemplate restTemplate = provider.getRestTemplate();
+		
+		Map<String, Object> urlVariables = new HashMap<String, Object>();
+		urlVariables.put("username", username);
+		
+//		restTemplate.get(RequestProvider.GET_USER_URL, requestEntity, urlVariables);
+		ResponseEntity<User> userEntity = restTemplate.getForEntity(RequestProvider.GET_USER_URL, User.class, urlVariables);
+		
+		
+		User user = new User();
+		user.setSortCode(userEntity.getBody().getSortCode());
+		
+		return user;
+		
 	}
 
 }
---
+++ SmartShopper/src/at/jku/smartshopper/backend/RequestProvider.java
@@ -0,0 +1,58 @@
+package at.jku.smartshopper.backend;
+
+import java.util.Collections;
+
+import org.springframework.http.HttpAuthentication;
+import org.springframework.http.HttpBasicAuthentication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+public class RequestProvider {
+	
+	
+	private static String PROTOCOL = "https";
+	private static String IP_ADDRESS = "10.0.2.2";
+	private static String PORT = "50443";
+	private static String SERVICE_NAME = "smartshopper.backend";
+	
+	private static String SERVICE_URL = PROTOCOL + "://" + IP_ADDRESS + ":" + PORT + "/" + SERVICE_NAME;
+	
+	public static String PUT_BASKET_URL = SERVICE_URL + "/basket/{username}/basket/{timestamp}";
+	public static String GET_ALL_BASKETS_URL = SERVICE_URL + "/basket/{username}/basket/all";
+	public static String GET_LATEST_BASKET_URL = SERVICE_URL + "/basket/{username}/basket/latest";
+		
+	public static String GET_USER_URL = SERVICE_URL + "/user/{username}";
+	
+	public static String GET_ARTICLE_URL = SERVICE_URL + "/article/{barcode}";
+	
+	
+//	public static String PUT_BASKET_URL = "https://192.168.30.224:50443/smartshopper.backend/basket/{username}/basket/{timestamp}/";
+//	public static String GET_ALL_BASKET_URL = "https://192.168.30.224:50443/smartshopper.backend/basket/{username}/basket/all/";
+	
+	
+	public HttpHeaders getHttpHeader() {
+//		String username = UserInstance.getUser().getUsername();
+//		String password = UserInstance.getUser().getPassword();
+		String username = "smartshopper";
+		String password = "smart";
+		HttpAuthentication authHeader = new HttpBasicAuthentication(username, password);
+		HttpHeaders requestHeaders = new HttpHeaders();
+		requestHeaders.setAuthorization(authHeader);
+		return requestHeaders;
+	}
+	
+	public RestTemplate getRestTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+		messageConverter.setSupportedMediaTypes(Collections
+				.singletonList(new MediaType("application", "json")));
+		restTemplate.getMessageConverters().add(messageConverter);
+		return restTemplate;
+	}
+	
+	
+	
+
+}
