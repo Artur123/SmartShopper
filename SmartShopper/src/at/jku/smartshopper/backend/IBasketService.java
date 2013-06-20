62952d5db7d7d2ef763473456673ee19d83d7f8d
Katharina <k.kaiser1@hotmail.com>
2013-06-20 15:05:45 +0200

    finished

--- SmartShopper/src/at/jku/smartshopper/backend/IBasketService.java
+++ SmartShopper/src/at/jku/smartshopper/backend/IBasketService.java
@@ -8,10 +8,26 @@ import at.jku.smartshopper.objects.BasketList;
  *
  */
 public interface IBasketService {
-	
+	/**
+	 * Saves a Basket on the Server
+	 * @param basket
+	 */
 	public void putBasket(Basket basket);
+	/**
+	 * Gets all Baskets from the Server
+	 * @return
+	 */
 	public BasketList getAllBaskets();
+	/**
+	 * 
+	 * @return
+	 */
 	public Basket getLatestBasket();
+	/**
+	 * 
+	 * @param timeStamp
+	 * @return
+	 */
 	public Basket getBasketById(long timeStamp);
 
 }
